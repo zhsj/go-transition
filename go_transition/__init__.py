81ead5b9b5ba314135921c10ac74c47199013df1
Shengjing Zhu <zhsj@debian.org>
2019-07-31 00:52:11 +0800

    add chardet to third_party

--- go_transition/__init__.py
+++ go_transition/__init__.py
@@ -3,6 +3,7 @@ from os.path import abspath, join, dirname
 
 sys.path.extend(
     [
+        abspath(join(dirname(__file__), "..", "third_party", "chardet")),
         abspath(join(dirname(__file__), "..", "third_party", "debian", "lib")),
         abspath(join(dirname(__file__), "..", "third_party", "mako")),
     ]
